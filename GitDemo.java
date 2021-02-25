6414d776d9f1cacd6e062580f61f57ad008d1bac
SDLahti <79568795+SDLahti@users.noreply.github.com>
2021-02-25 15:01:01 -0700

    Moved findFactorial() and reverseNumber
    Swapped the entire methods because I wasn't getting a merge conflict.

--- GitDemo.java
+++ GitDemo.java
@@ -18,6 +18,21 @@ public class GitDemo {
         previousNumber += num;
         return 0;
     }
+
+     /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+    public double reverseNumber(int num){
+        double newNumber = 1;
+        
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
+        }
+        reverseNumber = newNumber;        
+        return newNumber;
+    }
     
     /*****
      * This function should calculate the factorial of 'num' and return the resulting value.
@@ -34,21 +49,6 @@ public class GitDemo {
         }
         
         return factorialNumber;
-    }
-    
-    /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
-        double newNumber = 1;
-        
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
-        }
-        reverseNumber = newNumber;        
-        return newNumber;
     }
     
 }
